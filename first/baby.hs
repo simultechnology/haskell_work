21b8f8154205db6b16bc0b5f0146b6861208765a
takatsugu ishikawa <tissi0708@gmail.com>
2013-08-26 02:15:22 +0900

    add some functions

--- first/baby.hs
+++ first/baby.hs
@@ -1,1 +1,12 @@
 doubleMe x = x + x
+
+doubleUs x y = x * 2 + y * 2
+
+doubleSmallNumber x = if x > 100 
+                        then x 
+                        else x * 2
+                        
+doubleSmallNumber' x = (if x > 100 then x else x * 2) + 1
+
+lostNumbers = [4,8,15,16,23,42]
+
